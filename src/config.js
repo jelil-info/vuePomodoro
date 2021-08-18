6963dbc80e7fac498ddf4ed80c012f3b83de6970
Oyekanmi Jelili Oyetunji <oyekanmijelili@gmail.com>
2021-08-18 23:08:18 +0100

    I changed time

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-let workingTime = process.env.NODE_ENV === 'testing' ? 0.1 * 60 : 5 * 60
+let workingTime = process.env.NODE_ENV === 'testing' ? 0.1 * 60 : 1 * 60
 export const WORKING_TIME = workingTime
-export const RESTING_TIME = 5 * 60
+export const RESTING_TIME = 1 * 60
 export const KITTEN_TIME = 5
